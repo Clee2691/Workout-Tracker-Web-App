5b6452a6cc321c70560e6d2ed2c69a09e627e01e
clee2691 <lee.calv@northeastern.edu>
2022-03-11 15:00:24 -0800

    rough draft nav bar

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -3,8 +3,32 @@ import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg">
-            <Link to="/" className="navbar-brand">Workout Tracker</Link>
+        <nav className='navbar navbar-expand-sm navbar-dark bg-primary'>
+            <div className='container-fluid ms-2 me-2'>
+                <Link to='/' className='navbar-brand'>SwoleMate</Link>
+
+                <div className='collapse navbar-collapse' id="navbarNav">
+                    <ul class='navbar-nav me-auto'>
+                        <li className='nav-item'>
+                            <Link to='/home' className='nav-link active'>Home</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/search' className='nav-link'>Search</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/profile' className='nav-link'>Profile</Link>
+                        </li>
+                    </ul>
+                    <div className='d-flex'>
+                        <button className='btn btn-outline-success'>Login/Logout</button>
+                    </div>
+                </div>
+
+                <button class="navbar-toggler mt-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+            </div>
         </nav>
     )
 }
