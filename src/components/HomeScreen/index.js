b42efdb08112cde3008f7611b5fc371523649200
clee2691 <lee.calv@northeastern.edu>
2022-03-17 23:19:35 -0700

    Added workout logging

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import NavigationBar from '../NavigationBar';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import NavigationBar from "../NavigationBar";
+import ProfileWorkouts from "../ProfileScreen/ProfileWorkouts";
 
 const HomeScreen = () => {
-    return (
+  const loggedIn = true;
+  return (
+    <>
+      <NavigationBar currScreen={"HOME"} />
+      {!loggedIn && (
         <>
-            <NavigationBar currScreen={"HOME"}/>
-            <div className="banner-logo">
-                <div className="background-banner"/>
-                <h1 className="home-heading text-center">SwoleMate's Workout Log</h1>
-            </div>
-            
-            <div className="container">
-                <h1>Recent User Workouts</h1>
-            </div>
-            
-        
+          <div className="banner-logo">
+            <div className="background-banner" />
+            <h1 className="home-heading text-center">
+              SwoleMate's Workout Log
+            </h1>
+            <h2 className="subtitle-heading text-center">
+              Log in or sign-up to start tracking your workouts! In the
+              meantime, checkout other user's and their number of workouts!
+            </h2>
+          </div>
+
+          <div className="container">
+            <h1>Recent User Workouts</h1>
+          </div>
         </>
-    )
-}
+      )}
+      {loggedIn && (
+        <>
+          <div className="banner-logo">
+            <div className="background-banner" />
+            <h1 className="home-heading text-center">
+              Welcome back Calvin! Scroll down to start logging your workouts!
+            </h1>
+          </div>
+          <div className="container mt-2 col-md-8 mb-2">
+            <ProfileWorkouts/>
+          </div>
+          
+        </>
+      )}
+    </>
+  );
+};
 
 export default HomeScreen;
---
+++ src/components/WorkoutLog/index.js
@@ -0,0 +1,179 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import NavigationBar from "../NavigationBar";
+
+const WorkoutLog = () => {
+  const dispatch = useDispatch();
+
+  const todaysDate = new Date();
+  const dateStr =
+    todaysDate.getFullYear() +
+    "-" +
+    String(todaysDate.getMonth() + 1).padStart(2, 0) +
+    "-" +
+    String(todaysDate.getDate()).padStart(2, 0);
+
+  const [workoutSets, setWkSets] = useState([{_id: new Date().getTime(), set: 0, weight: 0, reps: 0 }]);
+
+  const [wkoutInfo, setWkoutInfo] = useState({
+    exName: "",
+    date: dateStr,
+    sets: workoutSets,
+  });
+
+  const wkouttSetinpChngHdler = (event, index) => {
+    const { name, value } = event.target;
+    const currSets = [...workoutSets];
+    // Add a new set
+    currSets[index][name] = value;
+    setWkSets(currSets);
+  };
+
+  // Handles exercise name and date changes
+  const nameDateInputHandler = (event) => {
+    const { name, value } = event.target;
+    const newWorkout = {
+      ...wkoutInfo,
+      [name]: value,
+    };
+    setWkoutInfo(newWorkout);
+  };
+
+  const addButtonHandler = (index) => {
+    setWkSets([...workoutSets, {_id: new Date().getTime(), set: index + 1, weight: 0, reps: 0 }]);
+  };
+
+  const removeBtnHandler = (index) => {
+    const currSets = [...workoutSets];
+    const selectedSet = currSets[index];
+    const filtered = currSets.filter((set) => set._id !== selectedSet._id);
+    setWkSets(filtered);
+  };
+
+  const saveWorkoutBtnHandler = () => {
+    const combInfo = {
+      _id: new Date().getTime(),
+      ...wkoutInfo,
+      sets: workoutSets,
+    };
+    console.log(combInfo);
+    dispatch({
+      type: "add-workout",
+      combInfo,
+    });
+    alert("Workout Successfully Added!");
+  };
+
+  return (
+    <>
+      <NavigationBar active={"Workout-Log"} />
+      <div className="container">
+        <h1 className="text-center mt-2">Workout Tracker</h1>
+        <div className="col-md-8 ms-auto me-auto">
+          <div className="input-group mb-2 mt-2">
+            <label className="form-label me-2" htmlFor="exerciseNameInput">
+              Exercise
+            </label>
+            <input
+              name="exName"
+              type="text"
+              className="form-control"
+              id="exerciseNameInput"
+              placeholder="Squat, Bench Press, etc..."
+              onChange={nameDateInputHandler}
+            />
+          </div>
+          <div className="input-group mb-2">
+            <label className="form-label me-2" htmlFor="date">
+              Date
+            </label>
+            <input
+              name="date"
+              type="date"
+              className="form-control rounded-corner"
+              defaultValue={wkoutInfo.date}
+              onChange={nameDateInputHandler}
+            />
+          </div>
+          {workoutSets.map((aSet, arrayIndex) => {
+            return (
+              <div key={aSet._id}>
+                <div className="input-group mb-2" >
+                  <label className="form-label me-2" htmlFor="setInput">
+                    Set #
+                  </label>
+                  <input
+                    name="set"
+                    id="setInput"
+                    type="number"
+                    className="form-control me-2"
+                    min="1"
+                    defaultValue={arrayIndex + 1}
+                    onChange={(event) =>
+                      wkouttSetinpChngHdler(event, arrayIndex)
+                    }
+                  />
+                  <label className="form-label me-2" htmlFor="weightInput">
+                    Weight (lbs)
+                  </label>
+                  <input
+                    name="weight"
+                    id="weightInput"
+                    type="number"
+                    className="form-control me-2"
+                    min="0"
+                    defaultValue={aSet.weight}
+                    onChange={(event) =>
+                      wkouttSetinpChngHdler(event, arrayIndex)
+                    }
+                  />
+                  <label className="form-label me-2" htmlFor="repsInput">
+                    Reps
+                  </label>
+                  <input
+                    name="reps"
+                    id="repsInput"
+                    type="number"
+                    className="form-control"
+                    min="0"
+                    defaultValue={aSet.reps}
+                    onChange={(event) =>
+                      wkouttSetinpChngHdler(event, arrayIndex)
+                    }
+                  />
+                </div>
+                <div className="mb-2">
+                  {workoutSets.length - 1 === arrayIndex && (
+                    <button
+                      className="btn btn-primary me-2"
+                      onClick={() => addButtonHandler(arrayIndex)}
+                    >
+                      <i className="fa-solid fa-plus me-2"></i>Add Set
+                    </button>
+                  )}
+                  {workoutSets.length !== 1 && (
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => removeBtnHandler(arrayIndex)}
+                    >
+                      <i className="fa-solid fa-minus me-2"></i>Remove Set
+                    </button>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+
+          <div className="d-grid">
+            <button className="btn btn-success" onClick={saveWorkoutBtnHandler}>
+              Add workout
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WorkoutLog;
