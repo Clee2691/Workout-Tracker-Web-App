ca47bb0f06cdd00507a15ecc0e17a86e8335ce4c
Calvin Lee <clee2691@gmail.com>
2022-04-17 19:31:53 -0400

    debug

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -6,22 +6,30 @@ const initialState = { sensitiveInfo: {} };
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_LOGGED_USER:
+      console.log("Getting logged user");
+      console.log(action.foundUser);
       return action.foundUser;
 
     case LOGINUSER:
+     console.log("Logging in");
+     console.log(action.matchedUser);
       return action.matchedUser;
 
     case UPDATE_USER:
+      console.log("update user");
       return { ...state, ...action.newUser };
 
     case REGISTER:
+      console.log("register");
       return action.insertedUser;
 
     case LOGOUTUSER:
+      console.log("logout");
       localStorage.clear();
       return initialState;
 
     default:
+      console.log("default state");
       return state;
   }
 };
